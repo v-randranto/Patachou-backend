22b5b1cdd3b50c4e8e2fbf6d64c755fadcfb0716
v-randranto <verorand@yahoo.com>
2020-06-16 20:37:54 +0200

    correction login

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -9,28 +9,36 @@ const jwt = require('jsonwebtoken');
 const accountData = require('../access-data/accountData');
 
 /****************************************************************************************
- * 
+ *
  * Connexion d'un utilisateur :
  * - recherche en base de l'utilisateur
  * - vérification de son mot de passe
  * - génération d'un jeton d'authentification à renvoyer au client
- * 
+ *
  ****************************************************************************************/
 
 exports.checkAccount = (req, res) => {
   logging('info', base, req.sessionID, 'Starting login authentication...');
+
   const loginStatus = {
     error: false,
-    auth: false,
-    token: null,
-    id: '',
+    notFound: false,
+    authKO: false,
+    exp: false,
+    jwtKO: false,
   };
+  const returnData = {
+    status: loginStatus,
+    member: null,
+    token: null,
+  };
+
   let foundAccount;
   // Recherche du membre par son pseudo
   const param = {
-    query: {pseudo: req.body.pseudo},
-    fields: 'password salt'
-  }; 
+    query: { pseudo: req.body.pseudo },
+    // fields: 'password salt'
+  };
 
   accountData
     .findOne(req.sessionID, param)
@@ -41,6 +49,7 @@ exports.checkAccount = (req, res) => {
         return true;
       } else {
         logging('info', base, req.sessionID, `Account ${req.body.pseudo} not found !`);
+        loginStatus.notFound = true;
         return false;
       }
     })
@@ -53,9 +62,9 @@ exports.checkAccount = (req, res) => {
           foundAccount.password
         );
         if (passwordChecked) {
-          logging('info', base, req.sessionID, `Account ${req.body.pseudo} password checked !`
-          );
-          loginStatus.auth = true;
+          logging('info', base, req.sessionID, `Account ${req.body.pseudo} password checked !`);
+          // TODO vérifier date d'expiration (gestion mot de passe perdu)
+          
           try {
             const token = jwt.sign(
               {
@@ -68,35 +77,41 @@ exports.checkAccount = (req, res) => {
               { algorithm: 'HS256' }
             );
             logging('info', base, req.sessionID, `Account ${req.body.pseudo} token created`);
-            loginStatus.token = token;
-            loginStatus.id = foundAccount._id;
-            loginStatus.expiresIn = 3600;
+            loginStatus.jwtKO = true;
+            returnData.token = token;
+            foundAccount.password = '*';
+            foundAccount.salt = '*';
+            console.log('found account purgé', foundAccount)
+            returnData.member = foundAccount;
           } catch (error) {
             logging('error', base, req.sessionID, `Jwt signing failed on account ${req.body.pseudo} !`);
-            loginStatus.error = true;
+            loginStatus.jwtKO = true;
           }
         } else {
+          loginStatus.authKO = true;
           logging('error', base, req.sessionID, `Account ${req.body.pseudo} password mismatch !`);
-          loginStatus.auth = false;
+          
         }
       }
     })
     .catch((error) => {
-      logging('error', base, req.sessionID, `Getting account ${req.body.pseudo} failed ! ${error}`
-      );
+      logging('error', base, req.sessionID, `Getting account ${req.body.pseudo} failed ! ${error}`);
       loginStatus.error = true;
-      // res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).json(loginStatus);
     })
     .finally(() => {
       if (loginStatus.error) {
         logging('debug', base, req.sessionID, 'technical error');
         res.status(httpStatusCodes.INTERNAL_SERVER_ERROR).end();
-      } else if (loginStatus.auth) {
-        logging('debug', base, req.sessionID, 'login authorized');
-        res.status(httpStatusCodes.OK).json(loginStatus);
-      } else {
+      } else if (loginStatus.notFound) {
+        logging('debug', base, req.sessionID, 'not found');
+        res.status(httpStatusCodes.NOT_FOUND).json({loginStatus});
+      } else if ((loginStatus.authKO || loginStatus.jwtKO)) {        
         logging('debug', base, req.sessionID, 'login unauthorized');
-        res.status(httpStatusCodes.UNAUTHORIZED).end();
+        res.status(httpStatusCodes.UNAUTHORIZED).json({loginStatus});
+      } else { 
+        logging('debug', base, req.sessionID, 'login authorized');       
+        console.log('return data', returnData);
+        res.status(httpStatusCodes.OK).json(returnData);
       }
     });
 };
